acd9649b70390ba6e1746394c1401568acf05e8c
MuonNghiCode <minhquanpm1610@gmail.com>
2025-02-05 20:40:44 +0700

    fix post

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import LoginPage from "./pages/LoginPage/LoginPage";
 import ErrorPage from "./pages/ErrorPage/ErrorPage";
 import UserPage from "./pages/UserPage/UserPage";
 import PostPage from "./pages/PostPage/PostPage";
-import PostList from "./components/PostList";
 
 const App: React.FC = () => {
   const router = createBrowserRouter([
@@ -21,7 +20,7 @@ const App: React.FC = () => {
         },
         {
           path: "posts",
-          element: <PostList />,
+          element: <PostPage />,
         },
       ],
     },
